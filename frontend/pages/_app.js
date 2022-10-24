162500cb01a3281d2fd6a97f231b86823392ee46
vp0942 <vp0942@gmail.com>
2022-10-25 00:58:58 +0300

    vv

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,7 +4,7 @@ import Page from '../components/Page';
 export default function MyApp({ Component, pageProps }) {
   return (
     <Page>
-    {/**/}
+      {/**/}
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </Page>
