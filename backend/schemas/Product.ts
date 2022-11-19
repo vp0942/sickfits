555c6a88acd7272b4f946cce147b4727d4dda6a5
vv <vp0942@gmail.com>
2022-11-19 20:14:18 +0200

    from Ubuntu

--- backend/schemas/Product.ts
+++ backend/schemas/Product.ts
@@ -1,15 +1,23 @@
-import { integer, select, text } from '@keystone-next/fields';
+import { integer, relationship, select, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 
 // Named export used to make outoimports work a bit nicer
 export const Product = list({
   // access:
-  // ui
   fields: {
     name: text({ isRequired: true }),
     description: text({
       ui: {
         displayMode: 'textarea',
+      },
+    }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
       },
     }),
     status: select({
