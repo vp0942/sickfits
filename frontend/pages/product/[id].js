a588a716024df6eb7850723e5becc4966ce08deb
vv <vp0942@gmail.com>
2022-11-28 18:13:16 +0200

    from Ubuntu

---
+++ frontend/pages/product/[id].js
@@ -0,0 +1,12 @@
+// next.js understands the route /product/[id].js
+// and matches the template and pushes the [id]
+// or anything that comes after /product/....
+// in query prop in the SingleProductPage component.
+// id is passed via {props} as query.id to the
+// SingleProduct component
+
+import SingleProduct from '../../components/SingleProduct';
+
+export default function SingleProductPage({ query }) {
+  return <SingleProduct id={query.id} />;
+}
