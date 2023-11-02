b18d15613fe066d780d23d0931e7bf495b026058
vv <vp0942@gmail.com>
2023-11-02 19:13:53 +0200

    from Ubuntu

--- frontend/__tests__/Product.test.js
+++ frontend/__tests__/Product.test.js
@@ -3,7 +3,8 @@ import Product from '../components/Product';
 import { fakeItem } from '../lib/testUtils';
 // Mocked provider is a component that mocks the Apollo provider
 import { MockedProvider } from '@apollo/client/testing';
-import priceTag from '../components/styles/PriceTag';
+
+// In the frontend word just check if the things render out appropriately!!!
 
 // Fake item from testUtils.js (in /lib)
 const product = fakeItem();
@@ -33,8 +34,31 @@ describe('<Product />', () => {
     // Test for a specific text
     // expect(link).toHaveTextContent('dogs are best');
     expect(link).toHaveTextContent(product.name);
+  });
 
-    // In the frontend word just check if the things render out appropriately
+  // Create a Jest snapshot
+  it('Renders and matches the snapshot', () => {
+    // The container is just an HTML element that contains all the rendered elements
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
 
+  // Testing Library Queries:
+  it('renders the image properly', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>,
+    );
+    // grab the image -> https://testing-library.com/docs/queries/about/
+    // In Product.js we have:
+    // <img src={product?.photo?.image?.publicUrlTransformed} alt={product?.name} />
+    // so we can grab the image by the alt text
+    const img = screen.getByAltText(product.name);
+    expect(img).toBeInTheDocument();
   });
 });
